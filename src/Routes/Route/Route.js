e340a11053fbce9c4a1258db27302d1e2a1a3e2d
mushfik16381 <ahmed.mushfik16381@gmail.com>
2022-11-08 20:17:51 +0600

    footer added

--- src/Routes/Route/Route.js
+++ src/Routes/Route/Route.js
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main";
 import Blog from "../../Pages/Blog/Blog";
 import Home from "../../Pages/Home/Home";
+import Login from "../../Pages/Login/Login";
+import Register from "../../Pages/Register/Register";
 
 export const routes = createBrowserRouter([
     {
@@ -15,6 +17,14 @@ export const routes = createBrowserRouter([
             {
                 path: '/blog',
                 element: <Blog></Blog>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/register',
+                element: <Register></Register>
             }
         ]
 
--- src/Shared/Footer/Footer.js
+++ src/Shared/Footer/Footer.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import { FaFacebook, FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa';
 import './Footer.css'
 
 const Footer = () => {
     return (
         <div className='footer-container'>
-            <div class="container d-flex justify-content-center py-5">
-      <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" >
-        <h2>jskdj</h2>
-      </button>
-    </div>
+                 <div class="container d-flex justify-content-center py-2">
+                 <button type="button" class="btn footer-icon" >
+                    <h4><FaFacebook/></h4>
+                    
+                </button>
+                 <button type="button" class="btn footer-icon" >
+                    <h4><FaInstagram/></h4>
+                </button>
+                 <button type="button" class="btn footer-icon" >
+                    <h4><FaTwitter/></h4>
+                </button>
+                 <button type="button" class="btn footer-icon" >
+                    <h4><FaYoutube/></h4>
+                </button>
+                </div>
             <p className='text-center mb-0 p-2' >Copyright &#169; 2022. All rights reserved by SquidFood</p>
         </div>
     );
